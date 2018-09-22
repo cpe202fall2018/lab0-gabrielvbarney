416e4bf5ec03b1086e1d9eb0d501ccba8c5f56d7
Gabriel Barney <gbarney@calpoly.edu>
2018-09-21 18:28:16 -0700

    6:28 Demo Lab0

--- planets.py
+++ planets.py
@@ -1,3 +1,14 @@
+#
+# Name:           Gabriel Barney
+# Student ID:   
+# Date:           9/21/18
+#
+# Lab:            0
+# Section:        6 (Hatalsky(Waitlist))/ 16 (Humer(Enrolled))
+# Purpose of Lab: To use basic python code to calculate a person's weight on Mars and Jupiter, as well as take inputs and display outputs. A refresher in basic python coding.
+# Additional Cmts:I did this lab first in Hatalsky's 10 AM lab (which I am waitlisted for) , and did it in about 5 minutes because I am very familiar with basic python. This lab was also assigned in Humer's 6PM lab, and I had already done it.
+
+
 def weight_on_planets():
     earth_weight = int(input("What do you weigh on earth? "))
     mars_weight = earth_weight * 0.38
